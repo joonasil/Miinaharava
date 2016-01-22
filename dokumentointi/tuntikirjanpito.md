7e268a5d44f7fed13fdc9e732191de9028b8a8db
joonasil <joonas.ilvonen@outlook.com>
2016-01-22 08:26:04 +0200

    Setup
    setting up java project and maven and pit test project

---
+++ dokumentointi/tuntikirjanpito.md
@@ -0,0 +1,2 @@
+20.01.2016 2h GitHubin, OTM ja javan kertausta.
+21.01.2016 2h
---
+++ minesweeper/src/minesweeper/Minesweeper.java
@@ -0,0 +1,9 @@
+package minesweeper;
+
+public class Minesweeper {
+
+    public static void main(String[] args) {
+    
+    }
+    
+}
